c9cbd95034daedc472f3f1d8e3dc0fb4e1bc0d92
Jordan Dunn <jordan@nodetwo.net>
2018-04-26 11:54:27 -0500

    No need for install_requires stuff

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='PhotoRename',
-    version='1.0.0',
+    version='1.0.2',
     author="Jordan Dunn",
     author_email="me@jordan-dunn.com",
     url="https://github.com/JorDunn/photorename",
     packages=['photorename', ],
     license='MIT',
-    long_description=open('README.md').read(),
-    install_requires=['argparse', 'hashlib']
+    long_description="A utility to rename photos and give them a more unified filename.",
+    install_requires=[]
 )
